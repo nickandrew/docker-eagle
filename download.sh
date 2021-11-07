68f85df028a0d2ed786a41a3317743ef96e14137
Nick Andrew <nick@nick-andrew.net>
2021-11-07 14:00:57 +1100

    Note no more auto-download

--- download.sh
+++ download.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Download latest EAGLE tarfile
+# It might not be possible to download from this script any more.
+#  See https://www.autodesk.com/products/eagle/free-download
 
-wget -O eagle.tar.gz https://www.autodesk.com/eagle-download-lin
+wget --referer=https://autodesk.com/products/eagle/free-download -O eagle.tar.gz https://www.autodesk.com/eagle-download-lin
